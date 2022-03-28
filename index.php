70a697b78bc950eed88604692f41464e58e49c11
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-29 01:07:57 +0200

    Inserito testo originale e testo filtrato

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 
-$text = "Io dico cacca quanto mi pare e piace, capito? Dov'è la libertà di parola, eh!? Sai che ti dico? cacca, cacca e stra-cacca HAHAHA!!!";
+$testo_originale = "Io dico cacca quanto mi pare e piace, capito? Dov'è la libertà di parola, eh!? Sai che ti dico? cacca, cacca e stra-cacca HAHAHA!!!";
 
-$textLength = strlen($text);
+$lunghezza_testo = strlen($testo_originale);
 
-echo $text;
+$parole_censurate = 0;
+
+$testo_filtrato = str_replace('cacca', '*****', $testo_originale, $parole_censurate);
+
+echo "Testo originale: ";
+echo "<br>";
+echo "<br>";
+echo $testo_originale;
+echo "<br>";
+echo "<br>";
+echo "Testo filtrato: ";
+echo "<br>";
+echo "<br>";
+echo $testo_filtrato;
+echo "<br>";
+echo "<br>";
 echo "<br>";
 echo "<br>";
 echo "Il testo è lungo: ";
-echo $textLength;
+echo $lunghezza_testo;
 echo " caratteri.";
+echo "<br>";
+echo "<br>";
+echo "Parole censurate: ";
+echo $parole_censurate;
+echo ".";
 
 ?>
